75742407fcac52a4970aad5af684ad1d3cd0602a
AshurW <ashurw@gmail.com>
2020-09-16 13:55:35 +0200

    fix

--- start.js
+++ start.js
@@ -1,6 +1,5 @@
 const app = require('./app')
 
 const PORT = process.env.PORT || 8080
-const IP = process.env.IP || '127.0.0.1'
 
-app.listen(PORT, IP, () => console.log('Connected to Server'))
+app.listen(PORT, () => console.log('Connected to Server'))
